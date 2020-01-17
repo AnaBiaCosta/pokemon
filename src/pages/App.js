7bbb2f8c67bff47c6723aea601ed9d84af13a30d
Guilherme Nunes Mingardi <guilherme.mingardi@easynvest.com.br>
2020-01-17 11:25:21 -0200

    Inicia uso de fetch

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../components/Card'
 import Teste from '../components/Teste'
 
-const App = () => (
-  <>
-  <Card
-    batata={Math.random() > 0.5}
-    text='Bom dia Mingardi'
-    src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRYXuCsAPfe-LcVuUiNGFlEx7JDeP_T6Ycb8iqIreurYuhQ8HM_'
-  />
+const App = () => {
+  const [text, setText] = useState('texto')
 
-  <Teste 
-    name='Mingadi'
-    description='Este é Minguardi'
-    src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6ZCW_Vq1R49qNAlfMryR16VYUJ8lpQvyLKG6ujYeW5BnxAKUz&s'/>
-  </>
-)
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/ditto/')
+      .then((res) => {
+        return res.json()
+      })
+      .then((res) => {
+        console.log(res)
+      })
+  }, [text])
+  return (
+    <>
+      <Card
+        batata={Math.random() > 0.5}
+        text={text}
+      />
+
+      <button onClick={() => setText('textao')}>SOCORRO</button>
+
+      <Teste
+        name='Mingadi'
+        description='Este é Minguardi'
+        src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6ZCW_Vq1R49qNAlfMryR16VYUJ8lpQvyLKG6ujYeW5BnxAKUz&s' />
+    </>
+  )
+}
 
 export default App;
