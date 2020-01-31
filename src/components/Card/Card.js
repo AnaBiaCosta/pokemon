94def6543408af0f95b1c4b46fe1bcd641fb39ef
Ana Beatriz de Sousa Costa <ana.costa@easynvest.com.br>
2020-01-31 11:12:32 -0300

    Adiciona busca de todos os pokemons

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Body, Image } from './Card.style'
 
-const Card = ({ src, name, description }) => (
+const Card = ({ src, name, description, setPokemon }) => (
     <Body >
         <h1>{name}</h1>
         <p>{description}</p>
-        <Image src={src}/>
+        <Image src={src}/>S
     </ Body>
 )
 
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,57 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import Card from '../components/Card'
-
 const App = () => {
-  const [pokemon, setPokemon] = useState({
-    nome: null,
-    image: null,
-  })
+  const [pokemons, setPokemons] = useState(null)
 
-  const [description, setDescription] = useState({
-    desc: null
-  })
-
+  const [description, setDescription] = useState(null)
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon/pikachu/')
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
       .then((res) => {
         return res.json()
       })
       .then((res) => {
-        setPokemon({
-          nome: res.name,
-          image: res.sprites.front_default
+        Promise.all(res.results.map(pokeObj => {
+          return fetch(pokeObj.url)
+            .then((poke) => poke.json())
+        })).then((values) => {
+          setPokemons(values)
         })
       })
   }, [])
-
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon-species/pikachu')
+    fetch('https://pokeapi.co/api/v2/pokemon-species/ditto')
+      .then((res) => res.json())
       .then((res) => {
-        return res.json()
-      })
-
-      .then((res) => {
-        const array = res.flavor_text_entries
-        
-        const findDesc = array.filter(item => { 
-          return item.version.name === 'red'
-        })
-
-        const result = findDesc[0].flavor_text
-
-        setDescription({
-          desc: result
-        })
+        setDescription(res && res.flavor_text_entries)
       })
   }, [])
-
+  const findDesc = (arr) => {
+    const result = arr && arr.filter(item => item.version.name === 'red')
+    return result && result[0].flavor_text
+  }
   return (
+    <>
     <Card
-      name={pokemon.nome}
-      src={pokemon.image}
-      description={description.desc}
+      // name={pokemons.nome}
+      // src={pokemons.image}
+      // description={findDesc(description)}
     />
+      {console.log(pokemons)}
+    </>
   )
 }
-
 export default App;
